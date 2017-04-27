0f1d5ce2403ad74c3fc7e4cbcfc36cab479170d0
Vish-Ram <busyvishy@gmail.com>
2017-04-26 19:07:49 -0700

    started writing training

--- src/model.py
+++ src/model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from config import *
+from utils import *
 
 #initial plan, set up Alice and Bob nets and the commpy BSC channel
 class BaseAgents(object):
@@ -31,24 +32,58 @@ class SimpleAgents(BaseAgents):
     def build_model(self):
         self.l1_transmitter = init_weights("transmitter_w_l1", [self.N, self.N])
         self.l2_transmitter = init_weights("transmitter_w_l2", [self.N, self.msg_len])
-        self.l1_receiver = init_weights("receiver_w_l1", [self.msg_len, self.N])
-        self.l2_receiver = init_weights("receiver_w_l2", [self.N, self.N])
+        self.l3_transmitter = init_weights("transmitter_w_l3", [self.msg_len, self.msg_len])
+        self.l1_receiver = init_weights("receiver_w_l1", [self.msg_len, self.msg_len])
+        self.l2_receiver = init_weights("receiver_w_l2", [self.msg_len, self.N])
+        self.l3_receiver = init_weights("receiver_w_l3", [self.N, self.N])
 
-        self.msg = self.placeholder("float", [None, self.N])
+        self.msg = tf.placeholder("float", [None, self.N])
 
         #transmitter network
-        #FC layer (block_len (N) x N) -> FC Layer (N x msg_len)
+        #FC layer (block_len (N) x N) -> FC Layer (N x msg_len) -> Output Layer (msg_len x msg_len)
         ##(not used yet) FC layer -> Conv layer
         self.transmitter_hidden_1 = tf.nn.sigmoid(tf.matmul(self.msg, self.l1_transmitter))
         self.transmitter_hidden_2 = tf.nn.sigmoid(tf.matmul(self.transmitter_hidden_1, self.l2_transmitter))
-        self.transmitter_output = tf.squeeze(self.transmitter_hidden_2)
+        self.transmitter_output = tf.squeeze(tf.nn.sigmoid(tf.matmul(self.transmitter_hidden_2, self.l3_transmitter)))
 
         #reciever network
-        #FC layer (msg_len x N) -> FC Layer (N x N)
+        #FC layer (msg_len x msg_len) -> FC Layer (msg_len x N) -> Output layer (N x N)
         ##(not used yet) Conv Layer -> FC Layer
         self.receiver_hidden_1 = tf.nn.sigmoid(tf.matmul(self.transmitter_output, self.l1_receiver))
         self.receiver_hidden_2 = tf.nn.sigmoid(tf.matmul(self.receiver_hidden_1, self.l2_receiver))
-        self.receiver_output = tf.squeeze(self.receiver_hidden_2)
+        self.receiver_output = tf.squeeze(tf.nn.sigmoid(tf.matmul(self.receiver_hidden_2, self.l3_receiver)))
+
+    def train(self):
+    	#Loss functions
+    	self.rec_loss = tf.reduce_mean(tf.abs(self.msg - self.receiver_output))
+
+    	#get training variables
+    	self.train_vars = tf.training_variables()
+    	self.trans_or_rec_vars = [var for var in self.train_vars if 'transmitter_' in var.name or 'receiver_' in var.name]
+
+    	#optimizers
+    	self.rec_optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(
+    							self.rec_loss, var_list=self.trans_or_rec_vars)
+
+    	self.rec_errors = []
+
+    	#training
+    	tf.initialize_all_variables().run()
+    	for i in range(self.epochs):
+    		iterations = 2000
+
+    		print('Training Transmitter and Receiver, Epoch:', i + 1)
+
+
+    def _train(self, network, iterations):
+    	rec_error = 1.0
+
+    	bs = self.batch_size
+
+    	for i in range(iterations):
+    		msg = gen_data(n=bs, block_len=self.block_len)
+
+    		
 
 
 class AdversaryAgents(BaseAgents):
